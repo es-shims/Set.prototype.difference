ae0d37f1bf8e9a3a352e9d6782de7add974b5c06
Jordan Harband <ljharb@gmail.com>
2024-02-06 15:07:15 -0800

    [Refactor] add `SetDataSize`
    See https://github.com/tc39/proposal-set-methods/pull/105

--- implementation.js
+++ implementation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Note: the commented out code is because there is no performant way in userland to do the optimization in step 7.
+// Note: the commented out code is because there is no performant way in userland to do the optimization in step 6.
 
 var $TypeError = require('es-errors/type');
 
@@ -13,6 +13,7 @@ var IteratorStep = require('es-abstract/2023/IteratorStep');
 var IteratorValue = require('es-abstract/2023/IteratorValue');
 var SameValueZero = require('es-abstract/2023/SameValueZero');
 // var SetDataHas = require('./aos/SetDataHas');
+var SetDataSize = require('./aos/SetDataSize');
 var ToBoolean = require('es-abstract/2023/ToBoolean');
 
 var callBound = require('call-bind/callBound');
@@ -22,7 +23,6 @@ var forEach = require('es-abstract/helpers/forEach');
 var tools = require('es-set/tools');
 var $setForEach = tools.forEach;
 var $setAdd = tools.add;
-var $setSize = tools.size;
 
 var $push = callBound('.Array.prototype.push');
 
@@ -38,33 +38,33 @@ module.exports = function difference(other) {
 
 	var otherRec = GetSetRecord(other); // step 3
 
-	var thisSize = $setSize(O); // step 5
+	var thisSize = SetDataSize(O); // step 5.a
 
 	var result = new $Set();
 
-	if (thisSize <= otherRec['[[Size]]']) { // step 6
-		$setForEach(O, function (e) { // step 6.a
-			var inOther = ToBoolean(Call(otherRec['[[Has]]'], otherRec['[[Set]]'], [e])); // step 6.a.i.1
-			if (!inOther) { // step 6.a.i.2 (kinda)
-				$setAdd(result, e); // step 6.a.i.2.a (kinda)
+	if (thisSize <= otherRec['[[Size]]']) { // step 5
+		$setForEach(O, function (e) { // step 5.a
+			var inOther = ToBoolean(Call(otherRec['[[Has]]'], otherRec['[[Set]]'], [e])); // step 5.a.i.1
+			if (!inOther) { // step 5.a.i.2 (kinda)
+				$setAdd(result, e); // step 5.a.i.2.a (kinda)
 			}
 		});
-	} else { // step 7
-		var keysIter = GetIteratorFromMethod(otherRec['[[Set]]'], otherRec['[[Keys]]']); // step 7.a
+	} else { // step 6
+		var keysIter = GetIteratorFromMethod(otherRec['[[Set]]'], otherRec['[[Keys]]']); // step 6.a
 		var resultSetData = [];
 		$setForEach(O, function (e) {
 			$push(resultSetData, e);
 		});
-		var next = true; // step 7.b
-		while (next) { // step 7.c
-			next = IteratorStep(keysIter); // step 7.c.i
-			if (next) { // step 7.c.ii
-				var nextValue = IteratorValue(next); // step 7.c.ii.1
-				if (nextValue === 0) { // step 7.c.ii.2
+		var next = true; // step 6.b
+		while (next) { // step 6.c
+			next = IteratorStep(keysIter); // step 6.c.i
+			if (next) { // step 6.c.ii
+				var nextValue = IteratorValue(next); // step 6.c.ii.1
+				if (nextValue === 0) { // step 6.c.ii.2
 					nextValue = +0;
 				}
 
-				// if (SetDataHas(resultSetData, nextValue)) { // step 7.c.ii.3
+				// if (SetDataHas(resultSetData, nextValue)) { // step 6.c.ii.3
 				for (var i = 0; i < resultSetData.length; i += 1) {
 					// eslint-disable-next-line max-depth
 					if (SameValueZero(resultSetData[i], nextValue)) {
@@ -82,10 +82,10 @@ module.exports = function difference(other) {
 		});
 	}
 
-	// var result = OrdinaryObjectCreate(%Set.prototype%, « [[SetData]] »); // step 8
+	// var result = OrdinaryObjectCreate(%Set.prototype%, « [[SetData]] »); // step 7
 
-	// result.[[SetData]] = resultSetData; // step 9
+	// result.[[SetData]] = resultSetData; // step 8
 
-	return result; // step 10
+	return result; // step 9
 
 };
